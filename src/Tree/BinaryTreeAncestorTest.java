d9e952c023115358853e1c21857e56842ef85a9e
Lida Li <lilida@lilida.org>
2012-09-18 22:59:40 -0700

    Add code for finding common ancestor for binary tree

---
+++ src/Tree/BinaryTreeAncestorTest.java
@@ -0,0 +1,9 @@
+package ProblemsSnippets.Tree;
+
+import junit.framework.TestCase;
+
+public class BinaryTreeAncestorTest extends TestCase {
+    public void testFindFirstCommonAncestor() throws Exception {
+
+    }
+}
